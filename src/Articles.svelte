b89cb862cf743f88a6b417342a728bdaca961cc7
Kamal Sahmoud <maroc.develop@gmail.com>
2019-11-15 12:07:05 +0100

    Add onDestroy

--- src/Articles.svelte
+++ src/Articles.svelte
@@ -1,49 +1,65 @@
 <script>
-	import store from './Store.js';
-	import Article from './Article.svelte';
+  import { onDestroy } from "svelte";
+  import store from "./Store.js";
+  import Article from "./Article.svelte";
 
-	let articles = [];
-	let title = "Title";
-	let author = "Author";
+  let articles = [];
+  let title = "Title";
+  let author = "Author";
 
-	// Get articles
-	store.subscribe(data => {
-		articles = data;
-	})
+  // Get articles
+  const unsubscribe = store.subscribe(data => {
+    articles = data;
+  });
 
-	// Add article
-	function addArticle() {
-		const newArticle = {
-			id: Date.now(),
-			title,
-			author
-		};
-		store.update(data => {
-			return [...data, newArticle];
-		})
-	}
+  // Add article
+  function addArticle() {
+    const newArticle = {
+      id: Date.now(),
+      title,
+      author
+    };
+    store.update(data => {
+      return [...data, newArticle];
+    });
+  }
 
-	// Remove article
-	function removeArticle(event) {
-		const articleToRemove = event.detail;
-		let filteredList = articles.filter(item => item.id !== articleToRemove.id)
-		store.set(filteredList);
-	}
+  // Remove article
+  function removeArticle(event) {
+    const articleToRemove = event.detail;
+    let filteredList = articles.filter(item => item.id !== articleToRemove.id);
+    store.set(filteredList);
+  }
+
+  // Destroy subscribe
+  onDestroy(unsubscribe);
 </script>
-
 
 <h3>Add new article</h3>
 <form on:submit|preventDefault={addArticle}>
-	<input type="text" name="title" id="title" placeholder="Title" bind:value={title} />
-	<br />
-	<input type="text" name="author" id="author" placeholder="Author" bind:value={author} />
-	<br />
-	<button type="submit">Add</button>
+  <input
+    type="text"
+    name="title"
+    id="title"
+    placeholder="Title"
+    bind:value={title} />
+  <br />
+  <input
+    type="text"
+    name="author"
+    id="author"
+    placeholder="Author"
+    bind:value={author} />
+  <br />
+  <button type="submit">Add</button>
 </form>
 
 <hr />
 
 <h3>List article</h3>
 {#each articles as article (article.id)}
-	<Article {article} on:removeArticle={removeArticle} on:addArticle={addArticle} />
+  <Article
+    {article}
+    on:removeArticle={removeArticle}
+    on:addArticle={addArticle} />
 {/each}
